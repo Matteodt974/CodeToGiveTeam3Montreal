64b464cf2280cb05d3cd2d0cbb4689ac3f10613e
Destriez--Terrighi, Mattéo <destriez_terrighi.matteo@courrier.uqam.ca>
2025-11-13 19:29:40 -0500

    first commit

---
+++ src/main/java/com/codetogive/codetogitteam3/CodeToGitTeam3Application.java
@@ -0,0 +1,13 @@
+package com.codetogive.codetogitteam3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeToGitTeam3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeToGitTeam3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codetogive/codetogitteam3/CodeToGitTeam3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetogive.codetogitteam3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeToGitTeam3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
